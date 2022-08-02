9a76cb596ad903878b2ab309044af358d351aed7
Viviana <vivianacg8@gmail.com>
2022-08-02 17:52:22 +0900

    added container from bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather />
-      </header>
-      <div>
-        <a href="https://github.com/castelv2/weather-react" target="_blank">
-          Open-source code
-        </a>
-        , by Viviana Castellanos
+      <div classsName="container">
+        <header className="App-header">
+          <Weather />
+        </header>
+        <div>
+          <a href="https://github.com/castelv2/weather-react" target="_blank">
+            Open-source code
+          </a>
+          , by Viviana Castellanos
+        </div>
       </div>
     </div>
   );
